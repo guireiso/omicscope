650136a00d05ca61b0cd8158d71852411b0f60b9
guireiso <guioliveirareis@gmail.com>
2022-11-21 10:33:26 -0300

    Input was all updated and tested

--- src/omics_teste.py
+++ src/omics_teste.py
@@ -1,4 +1,9 @@
-import omicscope as omics
+#import omicscope as omics
 
-teste = omics.Omicscope('C:\\Users\\Guilherme\\Desktop\\ALL.csv', ControlGroup= 'VEH1', Method = 'Progenesis')
+#teste = omics.Omicscope('C:\\Users\\Guilherme\\Desktop\\ALL.csv', ControlGroup= 'VEH1', Method = 'Progenesis')
 
+
+
+from omicscope.Input import Input
+patternLabNone = Input('..\\tests\\data\\proteins\\patternlab.xlsx', ControlGroup = None,
+            Method = 'PatternLab', filtering_method = 70)
