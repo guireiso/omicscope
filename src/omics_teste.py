e9123d5dbf4baaaca423bcba87698e2b0afbc6ee
guireiso <guioliveirareis@gmail.com>
2022-12-02 14:38:12 -0300

    bar_ident, volcano, heatmap

--- src/omics_teste.py
+++ src/omics_teste.py
@@ -4,9 +4,9 @@ from copy import copy
 import omicscope as omics
 
 teste = omics.Omicscope(Table = 'C:/Users/Guilherme/Desktop/ALL.csv',
-                        Method = 'Progenesis',
-                        ControlGroup= 'VEH',
-                        pdata = 'C:/Users/Guilherme/Desktop/pdata.xls',
-                        ExperimentalDesign= 'longitudinal', 
-                        pvalue = 'pvalue')
+                         Method = 'Progenesis',
+                         ControlGroup= None,                         pdata = 'C:/Users/Guilherme/Desktop/pdata.xls',
+                         ExperimentalDesign= 'longitudinal', 
+                         pvalue = 'pvalue')
 
+omics.heatmap(teste, c_cluster = False)
