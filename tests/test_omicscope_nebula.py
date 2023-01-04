04dac73e460fa0eb2c78bf0e125f5f4f7ae5b769
guireiso <guioliveirareis@gmail.com>
2023-01-04 16:56:56 -0300

    Nebula

---
+++ tests/test_omicscope_nebula.py
@@ -0,0 +1,7 @@
+from omicscope import *
+
+
+class Test(object):
+    def test_input_nebula(self):
+        df = Nebula('tests//data//MultipleGroups//omics_file')
+        assert len(df.groups) == 4
