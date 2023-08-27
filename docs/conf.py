088adcde3f5a688010238660e19dfd0e3cf630b2
Guilherme Reis-de-Oliveira <62401502+guireiso@users.noreply.github.com>
2023-08-27 16:22:02 -0300

    Bump version: 1.3.3 → 1.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ project = 'OmicScope'
 year = '2022'
 author = 'Guilherme Reis-de-Oliveira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.3.3'
+version = release = '1.3.4'
 
 pygments_style = 'trac'
 templates_path = ['.']
