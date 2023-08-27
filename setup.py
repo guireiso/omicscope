c484ae0a1c858ebc4fbd7b61b27ba12f6aa48ea7
Guilherme Reis-de-Oliveira <62401502+guireiso@users.noreply.github.com>
2023-08-27 17:01:47 -0300

    Bump version: 1.3.4 → 1.3.5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 
 setup(
     name='omicscope',
-    version='1.3.4',
+    version='1.3.5',
     license='MIT',
     description='OmicScope: from quantitative proteomics to systems biology.',
     long_description='{}\n{}'.format(
