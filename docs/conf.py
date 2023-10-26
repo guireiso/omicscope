cc5d48c73a9d67fc4cf0cca2ce3f53ae55e66c91
Guilherme Reis-de-Oliveira <62401502+guireiso@users.noreply.github.com>
2023-10-26 19:38:27 -0300

    config rtd

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/'))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "src")))
 
 extensions = [
     'sphinx.ext.autodoc',
