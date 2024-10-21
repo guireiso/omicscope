db07b521fd863f495c85dd95fb5d1867a29eec47
Guilherme Reis-de-Oliveira <guioliveirareis@gmail.com>
2024-10-20 22:29:45 -0300

    Bump version: 1.4.2 → 1.4.3

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ project = 'OmicScope'
 year = '2022'
 author = 'Guilherme Reis-de-Oliveira'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '1.4.2'
+version = release = '1.4.3'
 
 pygments_style = 'trac'
 templates_path = ['.']
