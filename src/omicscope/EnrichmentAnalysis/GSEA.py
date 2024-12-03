d2fbf01c5a29f36acb604ee9f6b98e48f277af30
Guilherme Reis-de-Oliveira <guioliveirareis@gmail.com>
2024-12-03 13:54:05 -0300

    prerank gsea

--- src/omicscope/EnrichmentAnalysis/GSEA.py
+++ src/omicscope/EnrichmentAnalysis/GSEA.py
@@ -41,51 +41,34 @@ class Enrichment:
     def gsea(self, db):
         """GSEA workflow according to gseapy
         """
-        from gseapy import gsea
-        # pvalue_cutoff = copy.copy(self.OmicScope.PValue_cutoff)
-        # foldchange_cutoff = copy.copy(self.OmicScope.FoldChange_cutoff)
+        from gseapy import prerank
         omics = copy.copy(self.OmicScope.quant_data)
-        conditions = [copy.copy(self.OmicScope.ControlGroup)]
-        conditions.extend(self.OmicScope.experimental)
-        conditions = conditions[::-1]
-        # GSEA DO NOT REQUIRE FILTERING STEPS TO ANALYSE DATA
-        # omics = omics.loc[(omics['log2(fc)'] <= -foldchange_cutoff) | (omics['log2(fc)'] >= foldchange_cutoff)]
-        # omics = omics[omics[self.OmicScope.pvalue] <= pvalue_cutoff]
+        omics = omics[['gene_name', "log2(fc)"]]
+        omics = omics.sort_values('log2(fc)',ascending=False).reset_index(drop=True)
         foldchange = dict(zip(omics.gene_name.str.upper(), omics['log2(fc)']))
-        gsea_input = omics.set_index('gene_name')
-        gsea_input = gsea_input.iloc[:, gsea_input.columns.str.contains('.', regex=False)]
-        gsea_input = np.log2(gsea_input)
-        gsea_input = gsea_input.replace(-np.inf, 0)
-        gsea_input = gsea_input.replace(np.nan, 0)
-        sorting_columns = [column for cond in conditions for column in gsea_input.columns if column.endswith(cond)]
-        gsea_input = gsea_input.reindex(columns=sorting_columns)
-        groups = gsea_input.columns.str.split('.', regex=False).str[1]
-        gsea_input = gsea_input.reset_index()
-        gsea_input['gene_name'] = gsea_input['gene_name'].str.upper()
-        gsea_input = gsea_input[gsea_input['gene_name'].notna()]
-        gsea_result = gsea(gsea_input, gene_sets=db,
-                           cls=groups, no_plot=True, outdir=None)
-        gsea_result = gsea_result.res2d
+        gsea_result = prerank(omics, gene_sets=db,
+                              no_plot=True, outdir=None,)
+        gsea_result = pd.DataFrame.from_dict(gsea_result.results).T
+        gsea_result = gsea_result.sort_values('fdr')
         gsea_result = gsea_result.reset_index()
-        gsea_result['DB'] = db
-        gsea_result = gsea_result.rename({'DB': 'Gene_set',
-                                          'Lead_genes': 'Genes',
-                                          'FDR q-val': 'Adjusted P-value',
-                                          'NOM p-val': 'P-value',
-                                          'Tag %': 'Overlap'},
-                                         axis='columns')
-
+        gsea_result = gsea_result.rename(columns={
+                                'index':'Term',
+                                'es':'ES',
+                                'nes':'NES',
+                                'pval':'P-value',
+                                'fdr':'Adjusted P-value',
+                                'fwerp':'FWER p-val',
+                                'tag %': 'Overlap',
+                                'gene %':'Gene %',
+                                'lead_genes':'Genes',                        
+                                })
+        gsea_result = gsea_result.reset_index()
+        gsea_result['Gene_set'] = db
         gsea_result = gsea_result[gsea_result['Adjusted P-value'] <= self.padjust_cutoff]
         gsea_result['Genes'] = gsea_result['Genes'].str.split(';')
         gsea_result['regulation'] = gsea_result['Genes'].apply(lambda x: [foldchange[i] for i in x if x != ['']])
         gsea_result['down-regulated'] = gsea_result['regulation'].apply(lambda x: len([i for i in x if i < 0]))
         gsea_result['up-regulated'] = gsea_result['regulation'].apply(lambda x: len([i for i in x if i > 0]))
         gsea_result['N_Proteins'] = gsea_result['down-regulated'] + gsea_result['up-regulated']
-        # try:
         gsea_result['-log10(pAdj)'] = gsea_result['Adjusted P-value'].apply(lambda x: -np.log10(x) if type(x) is float else 0)
-        # except TypeError:
-        #     gsea_result['-log10(pAdj)'] = 1
-        gsea_result = gsea_result[['Gene_set', 'Term', 'Overlap', 'P-value', 'Adjusted P-value',
-                                   'NES', 'ES', 'Genes', 'N_Proteins', '-log10(pAdj)', 'regulation',
-                                   'down-regulated', 'up-regulated']]
         return gsea_result
